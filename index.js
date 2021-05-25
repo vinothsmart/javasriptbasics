5aec5c816291677db31a317b701ecbfea0eb84b1
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-25 10:58:00 +0530

    javascript async

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // sync
 
-function otherFunc() {
-  console.log("we are now inside the function");
-  console.log("fetching the data from the API");
-}
+// function otherFunc() {
+//   console.log("we are now inside the function");
+//   console.log("fetching the data from the API");
+// }
 
+// console.log("start");
+
+// otherFunc();
+
+// console.log("end");
+
+// Async
 console.log("start");
 
-otherFunc();
+setTimeout(() => {
+  console.log("we are now in timeout");
+}, 2000);
 
 console.log("end");
