f2eda12d51eb3bf295bb8da1b9401ba3fb78a816
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-25 10:54:47 +0530

    javascript sync func

---
+++ index.js
@@ -0,0 +1,12 @@
+// sync
+
+function otherFunc() {
+  console.log("we are now inside the function");
+  console.log("fetching the data from the API");
+}
+
+console.log("start");
+
+otherFunc();
+
+console.log("end");
